fa292389312a4a28cca372ee36063f441c985864
chris <tarancello@gmail.com>
2018-09-11 11:01:30 -0400

    Started creation page, added links and empty actions/views

--- DnDTomeOfKeeping/Controllers/HomeController.cs
+++ DnDTomeOfKeeping/Controllers/HomeController.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace DnDTomeOfKeeping.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+            HttpWebRequest dndApiRequest = WebRequest.CreateHttp("http://www.dnd5eapi.co/api/classes");
+
+            dndApiRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+
+            HttpWebResponse dndApiResponse = (HttpWebResponse)dndApiRequest.GetResponse();
+
+            if (dndApiResponse.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader responseData = new StreamReader(dndApiResponse.GetResponseStream());
+
+                string data = responseData.ReadToEnd();
+
+                JObject jsonClasses = JObject.Parse(data);
+
+                ViewBag.Classes = jsonClasses["results"];
+            }
+
             return View();
         }
 
@@ -20,10 +41,47 @@ namespace DnDTomeOfKeeping.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult CharacterCreator(int Class)
         {
-            ViewBag.Message = "Your contact page.";
+            string classString = Class.ToString();
 
+            HttpWebRequest dndApiRequest = WebRequest.CreateHttp($"http://www.dnd5eapi.co/api/classes/{classString}");
+
+            dndApiRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+
+            HttpWebResponse dndApiResponse = (HttpWebResponse)dndApiRequest.GetResponse();
+
+            if (dndApiResponse.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader responseData = new StreamReader(dndApiResponse.GetResponseStream());
+
+                string data = responseData.ReadToEnd();
+
+                JObject jsonClasses = JObject.Parse(data);
+
+                ViewBag.Classes = jsonClasses;
+            }
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Tracker()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CCSearch()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
             return View();
         }
     }
